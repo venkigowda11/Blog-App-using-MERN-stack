ae3b8a12337de8ea05176dc1e23bb6dabbc01b76
venkigowda11 <venkateshmnvenki@gmail.com>
2023-12-05 01:40:26 +0530

    minor

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     credentials: true,
-    origin: "https://blog-app-frontend-three.vercel.app",
+    origin: ["https://blog-app-frontend-three.vercel.app"],
     methods: ["POST", "GET", "PUT"],
   })
 );
