761b8153cc4bf7e9dbb200cdc9a9a3cb0ad08fcc
venkigowda11 <venkateshmnvenki@gmail.com>
2023-12-05 02:20:32 +0530

    minor

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const path = require("path");
 const cheerio = require("cheerio");
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://blog-app-ten-ebon.vercel.app.com"
+  );
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
