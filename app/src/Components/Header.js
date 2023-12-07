5746ac4552c07c5e652312974afe0f15e5176571
venkigowda11 <venkateshmnvenki@gmail.com>
2023-12-08 00:05:49 +0530

    minor

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -6,16 +6,16 @@ import { useNavigate } from "react-router-dom";
 
 function Header() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
   const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
     fetch("https://blog-app-ten-ebon.vercel.app/profile", {
       credentials: "include",
     }).then((response) => {
-      response.json().then((userInfo1) => {
-        setUserInfo(userInfo1);
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
       });
     });
-    console.log("In");
   }, []);
 
   function logout() {
