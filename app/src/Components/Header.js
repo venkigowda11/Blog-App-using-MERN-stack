2d11560941a649fd5912deac9789fd803fd3d32b
venkigowda11 <venkateshmnvenki@gmail.com>
2023-12-07 23:59:25 +0530

    minor

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -8,7 +8,7 @@ function Header() {
   const navigate = useNavigate();
   const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
-    fetch("http://localhost:4000/profile", {
+    fetch("https://localhost:4000/profile", {
       credentials: "include",
     }).then((response) => {
       response.json().then((userInfo1) => {
