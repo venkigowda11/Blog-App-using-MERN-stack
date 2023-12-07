9ef7fede33fd9f6d71655d264a4cdffabe3c3f6f
venkigowda11 <venkateshmnvenki@gmail.com>
2023-12-08 00:48:37 +0530

    minor

--- server/index.js
+++ server/index.js
@@ -7,15 +7,6 @@ const multer = require("multer");
 const path = require("path");
 const cheerio = require("cheerio");
 
-app.use(
-  cors({
-    credentials: true,
-    origin: ["https://blog-app-frontend-three.vercel.app"],
-    methods: ["POST", "GET", "PUT"],
-    allowedHeaders: "Content-Type",
-  })
-);
-
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
@@ -25,6 +16,14 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+app.use(
+  cors({
+    credentials: true,
+    origin: ["https://blog-app-frontend-three.vercel.app"],
+    methods: ["POST", "GET", "PUT"],
+    allowedHeaders: "Content-Type",
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
