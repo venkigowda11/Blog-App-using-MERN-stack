b1460d60ac6ed5d58936ece74fed66ad1bd71e33
venkigowda11 <venkateshmnvenki@gmail.com>
2023-12-08 02:19:13 +0530

    logout

--- server/index.js
+++ server/index.js
@@ -176,6 +176,7 @@ app.put("/post", async (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("token").json("Logged out");
-  res.setHeader("Cache-Control", "no-store");
+  res.clearCookie("token");
+  res.setHeader("Cache-Control", "no-store"); // This line will execute
+  res.json("Logged out"); // This line will execute
 });
