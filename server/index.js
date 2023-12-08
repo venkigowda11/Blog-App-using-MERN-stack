238cc41d125dfae6e106ea31ad39b1ba488acc2d
venkigowda11 <venkateshmnvenki@gmail.com>
2023-12-08 15:00:42 +0530

    minor

--- server/index.js
+++ server/index.js
@@ -74,7 +74,7 @@ app.post("/login", async (req, res) => {
           res
             .cookie("token", token, {
               path: "/",
-              domain: "https://bloggerhub.vercel.app",
+              domain: "https://blog-server-coral.vercel.app",
             })
             .json({
               id: userDoc._id,
@@ -186,7 +186,7 @@ app.post("/logout", (req, res) => {
   res
     .clearCookie("token", {
       path: "/",
-      domain: "https://bloggerhub.vercel.app",
+      domain: "https://blog-server-coral.vercel.app",
     })
     .json("Logged out");
 });
