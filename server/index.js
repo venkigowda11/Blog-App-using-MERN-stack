7d1a0eb38f02b682488dddd4b7ece2b2dd4e662d
venkigowda11 <venkateshmnvenki@gmail.com>
2023-12-19 19:38:38 +0530

    minor

--- server/index.js
+++ server/index.js
@@ -10,18 +10,10 @@ app.use(cookieParser());
 const path = require("path");
 const cheerio = require("cheerio");
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://bloggerhub.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 app.use(
   cors({
     credentials: true,
-    origin: ["https://bloggerhub.vercel.app"],
     methods: ["POST", "GET", "PUT"],
-    allowedHeaders: "Content-Type",
   })
 );
 
