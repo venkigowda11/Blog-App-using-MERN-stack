0e71d4c6f0a46ea6e8c2c805cef483a4e38e871a
venkigowda11 <venkateshmnvenki@gmail.com>
2023-12-21 01:37:32 +0530

    minor

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -41,7 +41,6 @@ function Header() {
       });
       setUserInfo(null);
       navigate("/");
-      window.location.reload();
     } catch (error) {
       console.error("Logout error:", error);
     }
